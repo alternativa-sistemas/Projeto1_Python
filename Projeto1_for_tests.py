81d91274ea165e720130102cee33431265e913e4
Luiz Carlos <luiz@alternativasistemas.com.br>
2024-03-13 17:19:40 -0300

    +teste para garantir que no acerto imprima que foi acertado

--- Projeto1_for_tests.py
+++ Projeto1_for_tests.py
@@ -1,14 +1,17 @@
 import unittest
 import Projeto1_for_main
 
+
 class TestsProjeto1_for_main(unittest.TestCase):
 
     def test_digitar_numero_entre_1_e_50(self):
         print_list = []
         input_list = []
         input_output_list = ['0', '51', '3']
+
         def print1(text):
             print_list.append(text)
+
         def input1(text):
             input_list.append(text)
             return input_output_list.pop()
@@ -23,14 +26,34 @@ class TestsProjeto1_for_main(unittest.TestCase):
         print_list = []
         input_list = []
         input_output_list = ['1']
+
         def print1(text):
             print_list.append(text)
+
         def input1(text):
             input_list.append(text)
             return input_output_list.pop()
         Projeto1_for_main.main(print1, input1, len(input_output_list), 5)
         self.assertEqual(
             "['****************************************', 'Bem vindo ao meu joguinho de adivinhação', '****************************************', 'Teste: 1 de 1', 'Voce digitou: 1', 'Errou, seu chute foi menor do que o numero secreto.', '\\nFim de jogo!!!']",
+            str(print_list),
+            'programa está incorreto'
+        )
+
+    def test_digitar_numero_igual_ao_numero_secreto(self):
+        print_list = []
+        input_list = []
+        input_output_list = ['5']
+
+        def print1(text):
+            print_list.append(text)
+
+        def input1(text):
+            input_list.append(text)
+            return input_output_list.pop()
+        Projeto1_for_main.main(print1, input1, len(input_output_list), 5)
+        self.assertEqual(
+            "['****************************************', 'Bem vindo ao meu joguinho de adivinhação', '****************************************', 'Teste: 1 de 1', 'Voce digitou: 5', '\\n-----Acertou-----', '\\nFim de jogo!!!']",
             str(print_list),
             'programa está incorreto'
         )
